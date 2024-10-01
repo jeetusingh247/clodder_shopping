ef2cd603c235d4d824a59a1356f1a9de244b7afc
jeetusingh247 <jeetusingh11010@gmail.com>
2024-10-01 21:16:31 +0530

    updated widget theme

---
+++ clodder/lib/utils/theme/custom_themes/checkbox_theme.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+// Custom Class for Light & Dark Text Themes
+class JCheckboxTheme {
+  JCheckboxTheme._(); // To avoid creating instances
+
+  // Customizable Light Text Theme
+  static CheckboxThemeData lightCheckboxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(4),
+    ),
+    checkColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.white;
+      } else {
+        return Colors.black;
+      }
+    }),
+    fillColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.blue;
+      } else {
+        return Colors.transparent;
+      }
+    }),
+  );
+
+  // --> Dark Text Theme
+  static CheckboxThemeData darkCheckboxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(4),
+    ),
+    checkColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.white;
+      } else {
+        return Colors.black;
+      }
+    }),
+    fillColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.blue;
+      } else {
+        return Colors.transparent;
+      }
+    }),
+  );
+}
