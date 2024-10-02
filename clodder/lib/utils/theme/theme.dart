19dee5c706e4476fe6d3080e0ad5edebb1fde636
jeetusingh247 <jeetusingh11010@gmail.com>
2024-10-02 09:10:20 +0530

    Updated themes

--- clodder/lib/utils/theme/theme.dart
+++ clodder/lib/utils/theme/theme.dart
@@ -1,24 +1,48 @@
+import 'package:clodder/utils/theme/custom_themes/appbar_theme.dart';
+import 'package:clodder/utils/theme/custom_themes/bottom_sheet_theme.dart';
+import 'package:clodder/utils/theme/custom_themes/checkbox_theme.dart';
+import 'package:clodder/utils/theme/custom_themes/chip_theme.dart';
 import 'package:clodder/utils/theme/custom_themes/elevated_button_theme.dart';
+import 'package:clodder/utils/theme/custom_themes/outlined_button_theme.dart';
+import 'package:clodder/utils/theme/custom_themes/text_field_theme.dart';
 import 'package:clodder/utils/theme/custom_themes/text_theme.dart';
 import 'package:flutter/material.dart';
 
 class JAppTheme {
   JAppTheme._();
+
+  // --> Light Themes
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true,
     fontFamily: 'Poppins',
     brightness: Brightness.light,
     primaryColor: Colors.cyan,
-    scaffoldBackgroundColor: Colors.white,
     textTheme: JTextTheme.lightTextTheme,
+    chipTheme: JChipTheme.lightChipTheme,
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: JAppBarTheme.lightAppBarTheme,
+    checkboxTheme: JCheckboxTheme.lightCheckboxTheme,
+    bottomSheetTheme: JBottomSheetTheme.lightBottomSheetTheme,
     elevatedButtonTheme: JElevatedButtonTheme.lightElevatedButtonTheme,
+    outlinedButtonTheme: JOutlinedButtonTheme.lightOutlinedButtonTheme,
+    // --> it is a text field theme
+    inputDecorationTheme: JTextFormFieldTheme.lightInputDecorationTheme,
   );
+
+  // --> Dark Themes
   static ThemeData darkTheme = ThemeData(
     useMaterial3: true,
     fontFamily: 'Poppins',
     brightness: Brightness.dark,
     primaryColor: Colors.cyan,
-    scaffoldBackgroundColor: Colors.black,
     textTheme: JTextTheme.darkTextTheme,
+    chipTheme: JChipTheme.darkChipTheme,
+    scaffoldBackgroundColor: Colors.black,
+    appBarTheme: JAppBarTheme.darkAppBarTheme,
+    checkboxTheme: JCheckboxTheme.darkCheckboxTheme,
+    bottomSheetTheme: JBottomSheetTheme.darkBottomSheetTheme,
+    elevatedButtonTheme: JElevatedButtonTheme.darkElevatedButtonTheme,
+    outlinedButtonTheme: JOutlinedButtonTheme.darkOutlinedButtonTheme,
+    inputDecorationTheme: JTextFormFieldTheme.darkInputDecorationTheme,
   );
 }
